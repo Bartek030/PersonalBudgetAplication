625f344e9c94664b44589a6f2f742c86bc91adc1
Bartek <bartlomiejswies@gmail.com>
2021-02-03 21:38:00 +0100

    Sign in and sing up pages added

--- js/main.js
+++ js/main.js
@@ -1,27 +1,25 @@
-function countingDown()
-	{
-		let currentDate = new Date();
+function countingDown() {
+	let currentDate = new Date();
+	
+	let hour = currentDate.getHours();
+	if (hour<10) hour = "0"+hour;
+	
+	let minute = currentDate.getMinutes();
+	if (minute<10) minute = "0"+minute;
+	
+	let second = currentDate.getSeconds();
+	if (second<10) second = "0"+second;
+
+	let year = currentDate.getFullYear();
+
+	let month = currentDate.getMonth() + 1;
+	if (month<10) month = "0"+month;
+
+	let day = currentDate.getDay();
+	if (day<10) day = "0"+day;
+
+	$('#date').html("Data:&emsp;&nbsp;" + day + "-" + month + "-" + year);
+	$('#time').html("Godzina:&emsp;" + hour + ":" + minute + ":" + second);
 		
-		let hour = currentDate.getHours();
-		if (hour<10) hour = "0"+hour;
-		
-		let minute = currentDate.getMinutes();
-		if (minute<10) minute = "0"+minute;
-		
-		let second = currentDate.getSeconds();
-		if (second<10) second = "0"+second;
-
-		let year = currentDate.getFullYear();
-
-		let month = currentDate.getMonth() + 1;
-		if (month<10) month = "0"+month;
-
-		let day = currentDate.getDay();
-		if (day<10) day = "0"+day;
-
-		$('#date').html("Data:&emsp;&nbsp;" + day + "-" + month + "-" + year);
-		$('#time').html("Godzina:&emsp;" + hour + ":" + minute + ":" + second);
-		 
-		setTimeout("countingDown()",1000);
-	}
-
+	setTimeout("countingDown()",1000);
+}
