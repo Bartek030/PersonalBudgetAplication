9e61fa5272f62249a8c7f7a7a0117fabdd8bea4a
Bartek <bartlomiejswies@gmail.com>
2021-03-14 21:59:02 +0100

    Main menu page updated

--- js/main.js
+++ js/main.js
@@ -20,9 +20,21 @@ function setTime() {
 
 	$('#date').html(day + "-" + month + "-" + year);
 	$('#time').html(hour + ":" + minute + ":" + second);
-	$('#operationDate').val(year + '-' + month + '-' + day);
 		
 	setTimeout("setTime()",1000);
 }
 
+function setDate() {
+	let currentDate = new Date();
+	let year = currentDate.getFullYear();
+
+	let month = currentDate.getMonth() + 1;
+	if (month<10) month = "0"+month;
+
+	let day = currentDate.getDate();
+	if (day<10) day = "0"+day;
+	$('#operationDate').val(year + '-' + month + '-' + day);
+}
+
 setTime();
+setDate();
